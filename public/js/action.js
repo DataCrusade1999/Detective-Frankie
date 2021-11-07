b8390e12e38d9dbb3c27ae230ba95ee9fdc956b1
Restyled.io <commits@restyled.io>
2021-11-07 07:31:34 +0000

    Restyled by prettier

--- public/js/action.js
+++ public/js/action.js
@@ -11,12 +11,24 @@ export class Action {
     this.canvas = window.interactiveCanvas;
     this.gameScene = scene;
     this.commands = {
-      START_GAME : (params) => { this.gameScene.start(); },
-      CORRECT_ANSWER : (params) => { this.gameScene.correctAnswer(params); },
-      INCORRECT_ANSWER : (params) => { this.gameScene.incorrectAnswer(); },
-      WIN_GAME : (params) => { this.gameScene.winGame(params); },
-      LOSE_GAME : (params) => { this.gameScene.loseGame(); },
-      PLAY_AGAIN : (params) => { this.gameScene.start(); },
+      START_GAME: (params) => {
+        this.gameScene.start();
+      },
+      CORRECT_ANSWER: (params) => {
+        this.gameScene.correctAnswer(params);
+      },
+      INCORRECT_ANSWER: (params) => {
+        this.gameScene.incorrectAnswer();
+      },
+      WIN_GAME: (params) => {
+        this.gameScene.winGame(params);
+      },
+      LOSE_GAME: (params) => {
+        this.gameScene.loseGame();
+      },
+      PLAY_AGAIN: (params) => {
+        this.gameScene.start();
+      },
     };
   }
 
@@ -27,19 +39,22 @@ export class Action {
   setCallbacks() {
     // Declare the Interactive Canvas action callbacks.
     const callbacks = {
-      onUpdate : (data) => {
+      onUpdate: (data) => {
         try {
           const dataEntry = data[0];
-          const command = dataEntry.command  ? dataEntry.command
-                          : dataEntry.google ? dataEntry.google.intent.name
-                                             : null;
-          const params =
-              dataEntry.displayedWord ? dataEntry.displayedWord : null;
+          const command = dataEntry.command
+            ? dataEntry.command
+            : dataEntry.google
+            ? dataEntry.google.intent.name
+            : null;
+          const params = dataEntry.displayedWord
+            ? dataEntry.displayedWord
+            : null;
           this.commands[command.toUpperCase()](params);
         } catch (e) {
           // do nothing, when no command is sent or found
         }
-      }
+      },
     };
     // Called by the Interactive Canvas web app once web app has loaded to
     // register callbacks.
